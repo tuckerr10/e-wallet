88da5232431dc33eeca87de1d7420f9a071546a3
tuckerr10 <muhfrendy90@gmail.com>
2026-06-28 18:14:16 +0700

    feat: add local notification on successful topup

--- lib/presentation/pages/topup/topup_page.dart
+++ lib/presentation/pages/topup/topup_page.dart
@@ -7,6 +7,7 @@ import '../../blocs/payment/payment_bloc.dart';
 import '../../widgets/app_button.dart';
 import '../../widgets/app_top_bar.dart';
 import '../../widgets/feature_icon.dart';
+import '../../../core/utils/local_notification_helper.dart';
 
 class TopUpPage extends StatefulWidget {
   const TopUpPage({super.key});
@@ -30,6 +31,11 @@ class _TopUpPageState extends State<TopUpPage> {
     return BlocListener<PaymentBloc, PaymentState>(
       listener: (context, state) {
         if (state is PaymentTopupSuccess) {
+          LocalNotificationHelper.showNotification(
+            id: 2,
+            title: 'Top Up Berhasil',
+            body: 'Top up sebesar ${CurrencyFormatter.format(state.amount)} telah berhasil ditambahkan.',
+          );
           context.go('/success', extra: {
             'title': 'Top up berhasil',
             'subtitle': 'Saldo kamu bertambah',
